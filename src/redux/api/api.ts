6cfb27bc11de12c5e841c1cf540c5698e1a4efa7
RafidRisun <rafidhassanrisun@gmail.com>
2026-02-03 16:59:41 +0600

    Ratssss auaauaaaa

---
+++ src/redux/api/api.ts
@@ -0,0 +1,72 @@
+// import { storage } from '@/src/lib/mmkv_store';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { BaseQueryFn, createApi } from '@reduxjs/toolkit/query/react';
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+
+interface BaseQueryArgs extends AxiosRequestConfig {
+	url: string;
+	method?: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
+	body?: any;
+	headers?: Record<string, string>;
+}
+
+const baseQueryWithRath: BaseQueryFn<
+	BaseQueryArgs,
+	unknown,
+	unknown
+> = async args => {
+	// const token = AsyncStorage.getItem('token');
+	// const token = storage.getString('token');
+	// const token = await SecureStore.getItemAsync('authToken');
+	const token = await AsyncStorage.getItem('authToken');
+	console.log('REDUX', token);
+	//console.log('API REQUEST:', args);
+
+	try {
+		const result: AxiosResponse = await axios({
+			baseURL: 'http://10.10.10.65:8010/api',
+			...args,
+			url: args.url,
+			method: args.method,
+			data: args.body,
+			headers: {
+				...args.headers,
+				Authorization: token ? `Bearer ${token}` : '',
+			},
+		});
+		if (typeof result?.data === 'string') {
+			// if (!result.data.endsWith('}')) {
+			const withCurly = (result.data += '}');
+			return { data: JSON.parse(withCurly) };
+			// }
+		}
+		if (typeof result?.data === 'object') {
+			return { data: result?.data };
+		}
+
+		return { data: result?.data };
+	} catch (error: any) {
+		if (error.response?.data) {
+			if (typeof error.response?.data === 'string') {
+				const withCurly = (error.response.data += '}');
+				return { error: JSON.parse(withCurly) };
+			} else {
+				return { error: error.response?.data };
+			}
+		}
+		return {
+			error: {
+				status: error.response?.status || 500,
+				data: error.message || 'Something went wrong',
+			},
+		};
+	}
+};
+
+// Define the `createApi` with appropriate types
+export const api = createApi({
+	reducerPath: 'api',
+	baseQuery: baseQueryWithRath,
+	endpoints: () => ({}),
+	tagTypes: ['auth'],
+});
---
+++ src/redux/api/authApi/authApi.ts
@@ -0,0 +1,79 @@
+import { api } from '../api';
+
+export const authApi = api.injectEndpoints({
+	overrideExisting: true,
+	endpoints: builder => ({
+		userRegister: builder.mutation<any, any>({
+			query: data => ({
+				url: '/register',
+				method: 'POST',
+				body: data,
+			}),
+			invalidatesTags: ['auth'],
+		}),
+		otpVerify: builder.mutation<any, any>({
+			query: data => ({
+				url: '/verify-otp',
+				method: 'POST',
+				body: data,
+			}),
+			invalidatesTags: ['auth'],
+		}),
+		resendOtp: builder.mutation<any, any>({
+			query: data => ({
+				url: '/resend-otp',
+				method: 'POST',
+				body: data,
+			}),
+			invalidatesTags: ['auth'],
+		}),
+		forgotPassword: builder.mutation<any, any>({
+			query: data => ({
+				url: '/forgot-password',
+				method: 'POST',
+				body: data,
+			}),
+			invalidatesTags: ['auth'],
+		}),
+		login: builder.mutation<any, any>({
+			query: data => ({
+				url: '/login',
+				method: 'POST',
+				body: data,
+			}),
+			invalidatesTags: ['auth'],
+		}),
+		resetPassword: builder.mutation<any, any>({
+			query: data => ({
+				url: '/change-password',
+				method: 'POST',
+				body: data,
+			}),
+			invalidatesTags: ['auth'],
+		}),
+		validateToken: builder.mutation<any, any>({
+			query: data => ({
+				url: '/check-token?token=' + data,
+				method: 'POST',
+				body: data,
+			}),
+			invalidatesTags: ['auth'],
+		}),
+		logout: builder.mutation<any, void>({
+			query: () => ({
+				url: '/logout',
+				method: 'POST',
+			}),
+			invalidatesTags: ['auth'],
+		}),
+	}),
+});
+export const {
+	useUserRegisterMutation,
+	useOtpVerifyMutation,
+	useResendOtpMutation,
+	useLoginMutation,
+	useResetPasswordMutation,
+	useValidateTokenMutation,
+	useLogoutMutation,
+} = authApi;
